99669d7220c18dfa6f3ab9ac913c2f3a380e9d35
DarkOtter <34207844+DarkOtter@users.noreply.github.com>
2018-04-15 20:17:57 +0100

    Add direct indexing to bytes

--- src/words.rs
+++ src/words.rs
@@ -1,48 +1,61 @@
 use super::{div_64, mod_64, mult_64};
 use bits64::Bits64;
+use byteorder::{BigEndian, ByteOrder};
+
+fn read_partial_word(data: &[u8]) -> u64 {
+    if data.len() >= 8 {
+        panic!("Expected partial word, got at least a full word");
+    }
+    let mut res = [0u8; 8];
+    res[0..data.len()].copy_from_slice(data);
+    BigEndian::read_u64(&res)
+}
+
+pub fn read_word(data: &[u8], idx: usize) -> u64 {
+    let idx_bytes = idx << 3;
+    let end_idx_bytes = idx_bytes + 8;
+    if end_idx_bytes > data.len() {
+        read_word_last_word_case(data, idx_bytes)
+    } else {
+        BigEndian::read_u64(&data[idx_bytes..end_idx_bytes])
+    }
+}
+
+#[cold]
+fn read_word_last_word_case(data: &[u8], idx_bytes: usize) -> u64 {
+    if idx_bytes >= data.len() {
+        panic!("Index out of bounds");
+    }
+    debug_assert!((data.len() - idx_bytes) < 8);
+    read_partial_word(&data[idx_bytes..])
+}
+
+pub fn len_words(data: &[u8]) -> usize {
+    let byte_len = data.len();
+    let full_words = byte_len >> 3;
+    let partial_word = if (byte_len & 7) > 0 { 1 } else { 0 };
+    full_words + partial_word
+}
 
 pub trait WordData {
     fn len_words(&self) -> usize;
     fn get_word(&self, idx: usize) -> u64;
+
+    #[inline]
     fn count_ones_word(&self, idx: usize) -> u32 {
         self.get_word(idx).count_ones()
     }
 }
 
-fn get_bytes_for_word(data: &[u8], idx: usize) -> [u8; 8] {
-    let mut res = [0u8; 8];
-    let byte_idx = idx << 3;
-    let end_idx = byte_idx + 8;
-    if end_idx > data.len() {
-        if byte_idx >= data.len() {
-            panic!("Index out of bounds");
-        } else {
-            res[0..data.len() - byte_idx].copy_from_slice(&data[byte_idx..]);
-        }
-    } else {
-        res.copy_from_slice(&data[byte_idx..end_idx])
-    }
-    res
-}
-
 impl WordData for [u8] {
+    #[inline]
     fn len_words(&self) -> usize {
-        let byte_len = self.len();
-        let full_words = byte_len >> 3;
-        let partial_word = if (byte_len & 7) > 0 { 1 } else { 0 };
-        full_words + partial_word
+        len_words(self)
     }
 
     #[inline]
     fn get_word(&self, idx: usize) -> u64 {
-        use byteorder::{BigEndian, ByteOrder};
-        BigEndian::read_u64(&get_bytes_for_word(self, idx))
-    }
-
-    #[inline]
-    fn count_ones_word(&self, idx: usize) -> u32 {
-        use byteorder::{NativeEndian, ByteOrder};
-        NativeEndian::read_u64(&get_bytes_for_word(self, idx)).count_ones()
+        read_word(self, idx)
     }
 }
 
